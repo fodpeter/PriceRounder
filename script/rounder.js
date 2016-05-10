73f6750ced1b4583cceda4f7712bd84777e9030f
peti <fodpeter@gmail.com>
2016-05-10 21:23:03 +0200

    rounding up float numbers which end to 9

--- script/rounder.js
+++ script/rounder.js
@@ -1,19 +1,24 @@
 'use strict';
 
+function roundInt(num) {
+	if (num%10==9) {
+		return num+1;
+	}
+	return num;
+}
+
 function roundUp(text) {
 	var num = parseInt(text);
 	if (num == text) {
 		if (num==0) {
 			return text;
 		}
-		if (num%10==9) {
-			return num+1;
-		}
+		return roundInt(num);
 	}
 	num = parseFloat(text);
 	var rounded = Math.ceil(num);
 	if (rounded/num < 1.04) {
-		return rounded;
+		return roundInt(rounded);
 	}
 	return text;
 }
